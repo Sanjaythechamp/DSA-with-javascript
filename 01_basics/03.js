2af063bf9e469c6079da89fcf2cab4b800e0e620
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-03-24 09:39:52 +0000

    03_basics

---
+++ 01_basics/03.js
@@ -0,0 +1,19 @@
+const  age=16;
+
+if(age>=18){ 
+     
+     if(age<55){
+        console.log("you have a time ")
+     }
+    else if (age<=57){
+        console.log("eligible for job BUT RETRIEMENT SOON ")
+    }
+    else {
+        console.log("retriment Time ")
+    }
+ 
+  
+}
+else{
+    console.log("you are not eligible for job")
+}
