38f972b91d4b3a9c88196656bbe3c4f81677ed75
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-03-24 09:56:29 +0000

    switch

---
+++ 01_basics/switch.js
@@ -0,0 +1,34 @@
+// with the help of switch  we print day form monday to sunday
+
+// day are form 1 to 7 
+let day =66;
+
+switch(day){
+    case 1:
+        console.log("monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+        case 3:
+            console.log("wednesday")
+            break;
+         case 4:
+                console.log("Thrusday")
+                break;
+          case 5:
+                    console.log("friday")
+                    break;
+           case 6:
+                        console.log("saturday")
+                        break;  
+              case 7:
+                            console.log("sunday")
+                            break;
+                          
+
+              default:
+                console.log("please enter a valid value")  
+                break;
+        
+}
