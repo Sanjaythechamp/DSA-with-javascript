087ebd6fb2277c89297903c8f47d09aa81b6d570
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-03-24 09:17:29 +0000

    02_basics

---
+++ 01_basics/02.js
@@ -0,0 +1,15 @@
+let userInput = 7;
+
+
+if(userInput >= 80 && userInput <= 100) {
+    console.log("Student is in A grade");
+} 
+else if(userInput < 80 && userInput >= 50) {
+    console.log("Student is in B grade");
+} 
+else if(userInput < 50 && userInput >= 30) {
+    console.log("Student is in C grade");
+} 
+else {
+    console.log("Student is in D grade");
+}
