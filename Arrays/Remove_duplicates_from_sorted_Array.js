32c39cfce0653be8a509787eac3057538f4adca7
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-04-07 02:44:20 +0530

    Update Remove_duplicates_from_sorted_Array.js

--- Arrays/Remove_duplicates_from_sorted_Array.js
+++ Arrays/Remove_duplicates_from_sorted_Array.js
@@ -12,3 +12,18 @@ let arr = [1,2,3,3,3,4,4,5,5,5,6];
 console.log(values);
  })
 console.log(" The size of the set is ",res.size);
+
+// The Tc is ->> O(N)
+// how is this occur 
+
+/*
+
+Creating the Set (myset.add()): Adding an element to a Set
+has an average time complexity of O(1). Since you're adding each 
+element from the array to the Set once, this operation's time complexity is O(n),
+where n is the number of elements in the input array.
+
+Returning the Set (return myset): This operation doesn't involve any additional computation.
+It simply returns the Set. Its time complexity is O(1).
+
+*/
