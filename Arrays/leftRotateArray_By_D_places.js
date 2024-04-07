7eb1326aa41ebfc75a302c20332e13576b3b0359
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-04-07 13:01:37 +0000

    Arrays

--- Arrays/leftRotateArray_By_D_places.js
+++ Arrays/leftRotateArray_By_D_places.js
@@ -1,9 +1,9 @@
 
 function Reverse(arr,start,end){
-    while(start < end){
+    while(start <=end){
         let temp = arr[start];
-        arr[start] = arr[end - 1];
-        arr[end - 1] = temp;
+        arr[start] = arr[end ];
+        arr[end] = temp;
         start++;
         end--;
     }
@@ -13,8 +13,9 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let n = arr.length;
 let d = 3;
 
-Reverse(arr, 0, n - d);
-Reverse(arr, n - d, n);
-Reverse(arr, 0, n);
+Reverse(arr, 0, d-1);
+Reverse(arr, d, n-1);
+Reverse(arr, 0, n-1);
 
 console.log(arr);
+// Tc is -> O()
