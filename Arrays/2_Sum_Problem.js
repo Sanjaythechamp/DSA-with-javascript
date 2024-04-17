170b2780ec0a8872697e4496d1a9b5b0274de4a8
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-04-17 22:03:03 +0000

    Arrays

--- Arrays/2_Sum_Problem.js
+++ Arrays/2_Sum_Problem.js
@@ -1,5 +1,6 @@
 // Brute force 
 
+/*
 let arr = [8,5,6,2,11]
 let n = arr.length;
 let target_sum = 14;
@@ -23,5 +24,44 @@ for(let i=0;i<n;i++){
     }
 }
 
+
+*/
+
 // tc is O(n*n)
 // sc is O(1) 
+
+// BETTER SOLUTION :
+
+/*
+
+function twosum(arr,target,n){
+let hashMap = {};
+for(let i = 0;i<n;i++){
+
+    let a = arr[i];
+    let more = target - a ;
+    if(more in hashMap){
+        return ["yes",hashMap[more],i];
+    }
+    hashMap[a] = i;
+}
+return "No";
+
+}
+
+
+
+let  arr = [2,6,5,8,11];
+let n = arr.length;
+let target = 14;
+let res  = twosum(arr,target,n);
+console.log(`The value present  is ${res}`)
+
+// tc O(N)
+// sc O(N)
+
+
+*/
+
+
+// optimal solution  :: 
