6148e2879232fdbba84fb553c0edd6a707eba2e3
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-04-30 20:27:27 +0000

    Arrays

--- Arrays/Medium_Question/Longest_Consecutive_Sequence.js
+++ Arrays/Medium_Question/Longest_Consecutive_Sequence.js
@@ -75,6 +75,11 @@ let arr = [100, 200, 1, 2, 3, 4];
 let ans = longestSuccessiveElements(arr);
 console.log("The longest consecutive sequence is", ans);
 
+// tc is Time Complexity: O(NlogN) + O(N), N = size of the given array.
+//Reason: O(NlogN) for sorting the array. To find the longest sequence, we are using a loop that results in O(N).
+
+//Space Complexity: O(1), as we are not using any extra space to solve this problem.
+
 
 
 // optimal approach:
@@ -115,5 +120,16 @@ function longestSuccessiveElements(a) {
 let a = [100, 200, 1, 2, 3, 4];
 let ans = longestSuccessiveElements(a);
 console.log("The longest consecutive sequence is", ans);
+
+
+Time Complexity: O(N) + O(2*N) ~ O(3*N), where N = size of the array.
+Reason: O(N) for putting all the elements into the set data structure. After that for every starting element, we are finding the consecutive elements. Though we are using nested loops, the set will be traversed at most twice in the worst case. So, the time complexity is O(2*N) instead of O(N2).
+
+Space Complexity: O(N), as we are using the set data structure to solve this problem.
+
+Note: The time complexity is computed under the assumption that we are using unordered_set and it is taking O(1) for the set operations. 
+
+If we consider the worst case the set operations will take O(N) in that case and the total time complexity will be approximately O(N2). 
+And if we use the set instead of unordered_set, the time complexity for the set operations will be O(logN) and the total time complexity will be O(NlogN).
 
 */
