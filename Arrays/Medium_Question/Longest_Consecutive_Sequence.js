28e5aa2ef67e5e22319451f30142337e12d9e97d
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-04-30 20:20:39 +0000

    Arrays

--- Arrays/Medium_Question/Longest_Consecutive_Sequence.js
+++ Arrays/Medium_Question/Longest_Consecutive_Sequence.js
@@ -41,3 +41,42 @@ console.log(`The length of longest Subsequence length is ${res}`);
 
 // Better Approach :
 
+// optimal approach:
+
+
+
+
+function longestSuccessiveElements(a) {
+    let n = a.length;
+    if (n === 0)
+        return 0;
+
+    let longest = 1;
+    let st = new Set();
+    
+    // put all the array elements into set
+    for (let i = 0; i < n; i++) {
+        st.add(a[i]);
+    }
+
+    // Find the longest sequence
+    for (let it of st) {
+        // if 'it' is a starting number
+        if (!st.has(it - 1)) {
+            // find consecutive numbers
+            let cnt = 1;
+            let x = it;
+            while (st.has(x + 1)) {
+                x = x + 1;
+                cnt = cnt + 1;
+            }
+            longest = Math.max(longest, cnt);
+        }
+    }
+    return longest;
+}
+
+let a = [100, 200, 1, 2, 3, 4];
+let ans = longestSuccessiveElements(a);
+console.log("The longest consecutive sequence is", ans);
+
