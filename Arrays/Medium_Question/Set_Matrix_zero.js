8e4347e782fd7c1187f9efa0a447bb2631aeb9a0
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-05-16 18:43:59 +0000

    Arrays

---
+++ Arrays/Medium_Question/Set_Matrix_zero.js
@@ -0,0 +1,15 @@
+function matrix(){
+    let temp= [];
+    let no = 4;
+    for(let i=0;i<=5;i++){
+        
+        for(let j=0;j<=5;j++){
+            temp[i][j] = no ;
+            no++;
+        }
+    }
+    return temp;
+}
+let result = matrix();
+
+console.log(result);
