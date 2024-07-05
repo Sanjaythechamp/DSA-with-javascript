855f44121dfdd848130cc97f2cabe48d704f8141
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-07-05 13:42:51 +0000

    BinarySearch

--- BinarySearch/FindPeakElement.js
+++ BinarySearch/FindPeakElement.js
@@ -1,3 +1,4 @@
+/*
 function peakElement(){
     let arr = [1,2,3,1];
     let n = arr.length;
@@ -21,4 +22,48 @@ function peakElement(){
 let res= peakElement();
 console.log(res)
 
+
+
+*/
+
 // optimal approach 
+function peakElement2(){
+    let arr = [1,2,3,1];
+    let n = arr.length;
+    let min = -1;
+    if(arr[0]>arr[0+1] && arr[0]>min ){
+        return arr[0];
+
+    }
+    if(arr[n-1]>arr[n-2] && arr[n-1]> min){
+        return arr[n];
+    }
+    
+
+let low = 1;
+let high =  n-1; 
+        while(low<=high){
+            let mid = Math.floor((low+high)/2);
+
+            if(arr[mid]>arr[mid+1] && arr[mid]> arr[mid-1]){
+                return arr[mid]
+            }
+            else if(arr[mid]>arr[mid-1]){
+                  low = mid+1;
+            }
+            else if(arr[mid]<arr[mid+1]){
+                high  = mid-1
+            }
+    else{
+        // low = mid +1
+    }
+        
+        
+    }
+    return -1;
+
+}
+let res2 = peakElement2();
+console.log(res2)
+
+ 
