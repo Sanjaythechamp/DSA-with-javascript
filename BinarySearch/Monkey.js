bc678e3a82b94c2cf74437565a2ced858a638af2
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-07-07 12:33:59 +0000

    BinarySearch/js

---
+++ BinarySearch/Monkey.js
@@ -0,0 +1,51 @@
+ // Return the minimum interger[bananas/hour] ,such that koko can eat all bananas within h hours.
+
+
+ function hours_count(piles,mid){
+    let ans = 0;
+for(let i=0;i<piles.length;i++){
+
+     ans  =  ans + Math.ceil(piles[i]/mid);
+
+}
+
+return ans ;
+  
+
+}
+
+
+function koko(){
+
+    piles = [30,11,23,4,20];
+    let hours = 5;
+    let ans = 100000;
+    let low = 1;
+    let high = Math.max(...piles);   // O(N)
+    
+    while(low<=high){      // O(log2(max))
+        let mid = Math.ceil((low+high)/2);
+
+        let hours_take =  hours_count(piles,mid); // O(N)
+
+        if(hours_take <=hours){
+             ans = Math.min(ans,mid);
+             high = mid-1;
+              
+        }
+        else{
+            low= mid+1
+        }
+
+
+
+    }
+ return ans;
+
+}
+
+let result = koko();
+console.log('Minimum hours take ');
+console.log(result)
+
+ // Tc ->O(log2(max)) * O(N)
