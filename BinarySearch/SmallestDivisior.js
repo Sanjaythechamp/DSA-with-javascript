c4acb82bc17ae5ad53bc01a48a8143d95147c8d9
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-07-08 15:31:13 +0000

    BinarySearch

---
+++ BinarySearch/SmallestDivisior.js
@@ -0,0 +1,47 @@
+// find the smallest divisior given a threshold :
+
+function SumOfdivisior(arr,mid){
+let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        sum += Math.ceil(arr[i]/mid);
+
+    }
+
+    return sum ;
+
+}
+
+
+
+function divisior(){
+    let arr = [8,4,2,3];
+    let threshold = 10;
+
+let low = 1;
+let high = Math.max(...arr);
+
+while(low<=high){
+    let mid = Math.floor((low+high)/2);
+
+    let current_ans =  SumOfdivisior(arr,mid);
+      if(current_ans<=threshold){
+   
+        high = mid-1;
+
+
+      }
+      else{
+        low = mid+1;
+      }
+
+}
+return low;
+
+
+}
+
+let result = divisior();
+console.log(`smallest divisior is : ${result}`)
+
+// tc of this code is : O(logbase2(max))* O(N)
+// sc is O(1)
